d0caade6a52dfc51c27862e0fceb9e67f5bd3b0f
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2022-06-04 23:18:46 +0300

    Added static and views folder.
    Set the static and the views folder and added a main layout.

--- src/config/express.js
+++ src/config/express.js
@@ -1,3 +1,14 @@
+const handlebars = require('express-handlebars');
+
 module.exports = (app, express) => {
-  app.use(express.json());
+  // Setup handlebars
+  app.engine('hbs', handlebars.engine({ extname: 'hbs' }));
+  app.set('view engine', 'hbs');
+  app.set('views', './src/views');
+
+  // Setup static files
+  app.use(express.static('./src/public'));
+
+  // Setup body parser
+  app.use(express.urlencoded({ extended: true }));
 };
