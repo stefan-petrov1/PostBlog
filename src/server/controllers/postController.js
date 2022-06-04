69e7cf9548345b1e42f861ab61576c9a7ab79c4e
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2022-06-04 15:58:15 +0300

    Update postController.js
    Changed res.send to res.json :P

--- src/server/controllers/postController.js
+++ src/server/controllers/postController.js
@@ -3,7 +3,7 @@ const { postService } = require("../services/postService");
 async function getAll(req, res, next) {
   try {
     const posts = await postService.getAll();
-    res.send(posts);
+    res.status(200).json(posts);
   } catch (e) {
     next(e.message);
   }
