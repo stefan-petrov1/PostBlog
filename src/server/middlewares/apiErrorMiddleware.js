8041241abadd781dfc8c0a656e481a6d5e8a0a12
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2022-06-04 16:03:47 +0300

    Update apiErrorMiddleware.js
    Added some comments

--- src/server/middlewares/apiErrorMiddleware.js
+++ src/server/middlewares/apiErrorMiddleware.js
@@ -1,10 +1,12 @@
 const { ApiError } = require("../config/errors/ApiError");
 
+// Middleware to handle errors.
 exports.apiErrorMiddleware = (err, req, res, next) => {
   if (err instanceof ApiError) {
     return res.status(err.code).send(err);
   }
 
+  // Call the function again to give nice input to the frontend dev while still alerting the error to the backend
   console.error(`\n\n------ INTERNAL SERVER ERROR ------\n${err}\n\n`);
   this.apiErrorMiddleware(ApiError.internal(), req, res, next);
 };
