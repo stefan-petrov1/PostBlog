a9cd715dee49c93176b27df785ecb17c87091707
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2022-06-05 00:20:00 +0300

    Added login and register views.
    Added login and register views and updated the pageController and pageRouter so it can use the new views.

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -1,7 +1,17 @@
-function renderIndex(req, res, next) {
+function renderIndex(req, res) {
   res.render('index');
 }
 
+function renderLogin(req, res) {
+  res.render('login');
+}
+
+function renderRegister(req, res) {
+  res.render('register');
+}
+
 exports.pageController = {
-  renderIndex
+  renderIndex,
+  renderLogin,
+  renderRegister
 };
--- src/routers/pageRouter.js
+++ src/routers/pageRouter.js
@@ -3,5 +3,7 @@ const { pageController } = require('../controllers/pageController');
 const router = require('express').Router();
 
 router.get('/', pageController.renderIndex);
+router.get('/login', pageController.renderLogin);
+router.get('/register', pageController.renderRegister);
 
 exports.pageRouter = router;
