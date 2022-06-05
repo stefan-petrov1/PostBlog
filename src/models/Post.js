dd2bcd7392ed663168f542b8521735916bac62e9
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2022-06-05 15:01:07 +0300

    Update Post.js
    Added post validation

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-  title: { required: true, type: String },
-  description: { required: true, type: String },
+  title: {
+    required: true,
+    type: String,
+    minlength: [3, '{VALUE} must be at least 3 characters.'],
+    maxlength: [100, '{VALUE} mustn\'t be more than 100 characters.']
+  },
+  description: {
+    required: true,
+    type: String,
+    minlength: [10, '{VALUE} must be at least 10 characters.'],
+    maxlength: [500, '{VALUE} mustn\'t be more than 500 characters.']
+  },
   author: { required: true, type: mongoose.Types.ObjectId, ref: 'User' }
 });
 
