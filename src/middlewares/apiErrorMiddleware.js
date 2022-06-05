dd3e54d296b3fdda1e404793829c18b8c92c87ac
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2022-06-05 16:30:37 +0300

    Update apiErrorMiddleware.js

--- src/middlewares/apiErrorMiddleware.js
+++ src/middlewares/apiErrorMiddleware.js
@@ -6,7 +6,7 @@ exports.apiErrorMiddleware = (err, req, res, next) => {
     return res.render('error', err);
   }
 
-  // Call the function again to give nice input to the frontend dev while still alerting the error to the backend
+  // Call the function again to give nice input to the frontend while still alerting the error to the backend
   console.log(`\n\n------ INTERNAL SERVER ERROR ------\n`);
   console.error(err);
   console.log(`\n------ INTERNAL SERVER ERROR ------\n`);
